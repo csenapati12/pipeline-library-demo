a75019fb533fb1644a002c551bd09e7626f95e3f
Chaitanya Senapati <csenapati12@gmail.com>
2019-06-11 11:12:13 +0530

    Update addTwoNumber.groovy

--- vars/addTwoNumber.groovy
+++ vars/addTwoNumber.groovy
@@ -2,3 +2,11 @@ def call(int number1, int number2) {
   def addNumbers =number1+number2
   echo "Addition of two numbers is, ${addNumbers}."
 }
+def deploy(int number1, int number2) {
+  def addNumbers =number1+number2
+  echo "Addition of two numbers is, ${addNumbers}."
+}
+def creating(int number1, int number2) {
+  def addNumbers =number1+number2
+  echo "Addition of two numbers is, ${addNumbers}."
+}
